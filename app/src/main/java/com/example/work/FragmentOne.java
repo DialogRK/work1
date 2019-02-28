02e5735ea6b90da177a0357da5cfed9bf797eb74
gwl <1255754246@qq.com>
2019-02-28 23:40:20 +0800

    First commit.

---
+++ app/src/main/java/com/example/work/FragmentOne.java
@@ -0,0 +1,19 @@
+package com.example.work;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.work.R;
+
+
+public class FragmentOne extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_one,container,false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/work/FragmentThree.java
@@ -0,0 +1,20 @@
+package com.example.work;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.work.R;
+
+
+public class FragmentThree extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_three,container,false);
+        return view;
+    }
+}
